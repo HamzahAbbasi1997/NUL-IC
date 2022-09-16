e2aaebaea09edc80fbdd6f5443ec0636d4770101
Hamzah Abbasi <hamzahabbasi1997@gmail.com>
2022-09-16 16:00:06 +0100

    added info sections

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Texts',
+    headline: 'Insert a suitable headline in this space',
+    description: 'Donate now and help our website blah blah blah blah blah',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg').default,
+    alt:'flowers',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited Power',
+    headline: 'Insert a suitable headline in this space',
+    description: 'Donate now and help our website blah blah blah blah blah',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/svg-2.svg').default,
+    alt:'truck',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+
+export const homeObjThree = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Texts',
+    headline: 'Insert a suitable headline in this space',
+    description: 'Donate now and help our website blah blah blah blah blah',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images/svg-3.svg').default,
+    alt:'park',
+    dark: true,
+    primary: true,
+    darkText: false
+};
