2ebce4ea451a0a1496cfa112ea026c6f9131eb92
Hamzah Abbasi <hamzah.abbasi@student.manchester.ac.uk>
2023-08-20 20:35:33 +0100

    reverted to old link

--- src/pages/event.js
+++ src/pages/event.js
@@ -6,7 +6,7 @@ const Event = () => {
         <>
             {/* Embed Google Form */}
             <iframe title="Nulic Open Day"
-            src="https://docs.google.com/forms/d/e/1FAIpQLSeDVuLrIwWlF7w9WJCB952ZOHobJFBObCE6HhBeVIDhX9SYLA/viewform?embedded=true" 
+            src="https://docs.google.com/forms/d/e/1FAIpQLSdKFi1gaTz8-Z9LqztKfHL-eBfBK0Zn8z729vRIUxK8zCMZGg/viewform?embedded=true" 
             width="100%"   // Set the width to 100% for responsiveness
             height="900"   // Set the initial height for the form
             frameborder="0"
